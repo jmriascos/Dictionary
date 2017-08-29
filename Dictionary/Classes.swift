80d457b2a61cd13088d369a91279a15047180723
Jorge Mario Riascos <jmriascos@hotmail.com>
2017-08-29 17:13:22 -0500

    Arreglo de push en el modal y creación de static var

--- Dictionary/Classes.swift
+++ Dictionary/Classes.swift
@@ -9,11 +9,29 @@
 import Foundation
 import UIKit
 
-var language = "English"
+
 let sectionsEnglish = ["Terms", "Settings"]
 let sectionsSpanish = ["Términos", "Configuración"]
 
+class Language {
+    static var language = "English"
+    
+    func changeLanguage (language: String) {
+        var language = "English"
+        if language == "English" {
+            language = "Spanish"
+        }else {
+            if language == "Spanish" {
+                language = "English"
+            }
+        }
+    }
+    
+}
+
+
 class TableViewController: UITableViewController {
+    
     
     @IBOutlet weak var term1Label: UILabel!
     @IBOutlet weak var term2Label: UILabel!
@@ -23,8 +41,9 @@ class TableViewController: UITableViewController {
     @IBOutlet weak var changeLanguageButton: UILabel!
     
     
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch language {
+        switch Language.language {
         case "Spanish":
             switch section {
             case 0:
@@ -48,7 +67,7 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        switch language {
+        switch Language.language {
         case "Spanish":
             term1Label.text = "Clase"
             term2Label.text = "Herencia"
@@ -70,6 +89,9 @@ class TableViewController: UITableViewController {
     }
 }
 
+
+
+
 class ModalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -78,10 +100,12 @@ class ModalViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     @IBAction func spanishLanguageButton(_ sender: Any) {
-        language = "Spanish"
+        dismiss(animated: true, completion: nil)
+        Language.language = "Spanish"
     }
     @IBAction func englishLanguageButton(_ sender: Any) {
-        language = "English"
+        dismiss(animated: true, completion: nil)
+        Language.language = "English"
     }
 }
 
@@ -92,22 +116,23 @@ class Term1ViewController: UIViewController {
     @IBOutlet weak var termTitleLabel: UILabel!
     @IBOutlet weak var termImage: UIImageView!
     @IBOutlet weak var termText: UITextView!
-    //@IBOutlet weak var sourceWikipediaLabel: UILabel!
+    @IBOutlet weak var wikipediaLabel: UILabel!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.termImage.image = #imageLiteral(resourceName: "classImage")
-        switch language {
+        switch Language.language {
         case "Spanish":
             self.termTitleLabel.text = "Clases"
             self.termText.text = classSpanishText
-            //self.sourceWikipediaLabel.text = "Fuente Wikipedia"
+            self.wikipediaLabel.text = "Fuente Wikipedia"
             self.url = URL (string: "https://es.wikipedia.org/wiki/Clase_(informatica)")!
         default:
             self.termTitleLabel.text = "Class"
             self.termText.text = classEnglishText
-            //self.sourceWikipediaLabel.text = "Source Wikipedia"
+            self.wikipediaLabel.text = "Source Wikipedia"
             self.url = URL (string: "https://en.wikipedia.org/wiki/Class_(computer_programming)")!
         }
     }
@@ -127,22 +152,23 @@ class Term2ViewController: UIViewController {
     @IBOutlet weak var termTitleLabel: UILabel!
     @IBOutlet weak var termImage: UIImageView!
     @IBOutlet weak var termText: UITextView!
-    //@IBOutlet weak var sourceWikipediaLabel: UILabel!
+    @IBOutlet weak var wikipediaLabel: UILabel!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.termImage.image = #imageLiteral(resourceName: "inheritance")
-        switch language {
+        switch Language.language {
         case "Spanish":
             self.termTitleLabel.text = "Herencia"
             self.termText.text = inheritanceSpanishText
-      //      self.sourceWikipediaLabel.text = "Fuente Wikipedia"
+            self.wikipediaLabel.text = "Fuente Wikipedia"
             self.url = URL (string: "https://es.wikipedia.org/wiki/Herencia_(informatica)")!
         default:
             self.termTitleLabel.text = "Inheritance"
             self.termText.text = inheritanceEnglishText
-      //      self.sourceWikipediaLabel.text = "Source Wikipedia"
+            self.wikipediaLabel.text = "Source Wikipedia"
             self.url = URL (string: "https://en.wikipedia.org/wiki/Inheritance_(object-oriented_programming)")!
         }
     }
@@ -162,21 +188,22 @@ class Term3ViewController: UIViewController {
     @IBOutlet weak var termTitleLabel: UILabel!
     @IBOutlet weak var termImage: UIImageView!
     @IBOutlet weak var termText: UITextView!
-    //@IBOutlet weak var sourceWikipediaLabel: UILabel!
+    @IBOutlet weak var wikipediaLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.termImage.image = #imageLiteral(resourceName: "classMethod")
-        switch language {
+        switch Language.language {
         case "Spanish":
             self.termTitleLabel.text = "Metodo de Clase"
             self.termText.text = classMethodSpanishText
-    //        self.sourceWikipediaLabel.text = "Fuente Wikipedia"
+            self.wikipediaLabel.text = "Fuente Wikipedia"
             self.url = URL (string: "https://es.wikipedia.org/wiki/Metodo_(informatica)")!
         default:
             self.termTitleLabel.text = "Method"
             self.termText.text = classMethodEnglishText
-    //        self.sourceWikipediaLabel.text = "Source Wikipedia"
+            self.wikipediaLabel.text = "Source Wikipedia"
             self.url = URL (string: "https://en.wikipedia.org/wiki/Method_(computer_programming)#Class_methods")!
         }
     }
@@ -196,21 +223,22 @@ class Term4ViewController: UIViewController {
     @IBOutlet weak var termTitleLabel: UILabel!
     @IBOutlet weak var termImage: UIImageView!
     @IBOutlet weak var termText: UITextView!
-    //@IBOutlet weak var sourceWikipediaLabel: UILabel!
+    @IBOutlet weak var wikipediaLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.termImage.image = #imageLiteral(resourceName: "classesObjects")
-        switch language {
+        switch Language.language {
         case "Spanish":
             self.termTitleLabel.text = "Objeto"
             self.termText.text = classObjectSpanishText
-    //        self.sourceWikipediaLabel.text = "Fuente Wikipedia"
+            self.wikipediaLabel.text = "Fuente Wikipedia"
             self.url = URL (string: "https://es.wikipedia.org/wiki/Objeto_(programacion)")!
         default:
             self.termTitleLabel.text = "Class Object"
             self.termText.text = classObjectEnglishText
-    //        self.sourceWikipediaLabel.text = "Source Wikipedia"
+            self.wikipediaLabel.text = "Source Wikipedia"
             self.url = URL (string: "https://en.wikipedia.org/wiki/Object_(computer_science)")!
         }
     }
@@ -230,21 +258,22 @@ class Term5ViewController: UIViewController {
     @IBOutlet weak var termTitleLabel: UILabel!
     @IBOutlet weak var termImage: UIImageView!
     @IBOutlet weak var termText: UITextView!
-    //@IBOutlet weak var sourceWikipediaLabel: UILabel!
+    @IBOutlet weak var wikipediaLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.termImage.image = #imageLiteral(resourceName: "classVariable")
-        switch language {
+        switch Language.language {
         case "Spanish":
             self.termTitleLabel.text = "Variable de clase"
             self.termText.text = classVariableSpanishText
-    //        self.sourceWikipediaLabel.text = "Fuente Wikipedia"
+            self.wikipediaLabel.text = "Fuente Wikipedia"
             self.url = URL (string: "https://es.wikipedia.org/wiki/Variable_de_clase")!
         default:
             self.termTitleLabel.text = "Class Variable"
             self.termText.text = classVariableEnglishText
-    //        self.sourceWikipediaLabel.text = "Source Wikipedia"
+            self.wikipediaLabel.text = "Source Wikipedia"
             self.url = URL (string: "https://en.wikipedia.org/wiki/Class_variable")!
         }
     }
